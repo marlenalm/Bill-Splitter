dac5a6e11f62d5ca96fcb8a5f5056bee6c86e5db
Marlena Morshead <marlenamorshead@att.net>
2015-02-07 16:51:50 -0500

    Initial Commit

---
+++ Bill Splitter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Bill Splitter
+//
+//  Created by Marlena Morshead on 2/7/15.
+//  Copyright (c) 2015 Marlena Morshead. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
